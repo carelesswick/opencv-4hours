d836df017e5f070713f9b9292dd99149f12e2b74
LiYiFan <fan868552@gmail.com>
2026-05-26 22:29:13 +0800

    添加项目文件。

---
+++ opencv-4hours/Test.cpp
@@ -0,0 +1,19 @@
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+
+int main()
+{
+	string path = "Resource/test.png";
+	Mat img = imread(path);
+	imshow("Image",img);
+	waitKey(0);
+	return 0;
+
+
+}
